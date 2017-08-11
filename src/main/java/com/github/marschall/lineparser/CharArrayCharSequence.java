5ddf56615c46a078d1c1e459e4f1837fa1828f30
Philippe Marschall <philippe.marschall@gmail.com>
2017-08-11 07:08:38 +0200

    Work on better char[] support

--- src/main/java/com/github/marschall/lineparser/CharArrayCharSequence.java
+++ src/main/java/com/github/marschall/lineparser/CharArrayCharSequence.java
@@ -35,53 +35,104 @@ final class CharArrayCharSequence implements CharSequence {
     if ((start < 0) || (start > this.array.length) || (start > end) || (end > this.array.length)) {
       throw new IndexOutOfBoundsException();
     }
-    return new CharArrayCharSubSequence(this.array, start, end - start);
+    if (start == 0) {
+      return new CharArrayPrefixSubSequence(this.array, end);
+    }
+    return new CharArrayFullSubSequence(this.array, start, end - start);
   }
 
-  final class CharArrayCharSubSequence implements CharSequence {
 
-    private final char[] array;
-    private final int offset;
-    private final int count;
-    private String stringValue;
+}
 
-    CharArrayCharSubSequence(char[] array, int offset, int length) {
-      Objects.requireNonNull(array);
-      this.array = array;
-      this.offset = offset;
-      this.count = length;
+
+final class CharArrayPrefixSubSequence implements CharSequence {
+
+  private final char[] array;
+  private final int count;
+  private String stringValue;
+
+  CharArrayPrefixSubSequence(char[] array, int length) {
+    Objects.requireNonNull(array);
+    this.array = array;
+    this.count = length;
+  }
+
+  @Override
+  public String toString() {
+    if (this.stringValue == null) {
+      this.stringValue = new String(this.array, 0, this.count);
     }
+    return this.stringValue;
+  }
 
-    @Override
-    public String toString() {
-      if (this.stringValue == null) {
-        this.stringValue = new String(this.array, this.offset, this.count);
-      }
-      return this.stringValue;
-    }
+  @Override
+  public int length() {
+    return this.count;
+  }
 
-    @Override
-    public int length() {
-      return this.count;
-    }
+  @Override
+  public char charAt(int index) {
+    if (index >= this.count) {
+    throw new IndexOutOfBoundsException();
+  }
+    return this.array[index];
+  }
 
-    @Override
-    public char charAt(int index) {
-      if ((index < 0) || (index >= this.count)) {
+  @Override
+  public CharSequence subSequence(int start, int end) {
+    if ((start < 0) || (start > this.count) || (start > end) || (end > this.count)) {
       throw new IndexOutOfBoundsException();
     }
-      return this.array[this.offset + index];
+    if (start == 0) {
+      return new CharArrayPrefixSubSequence(this.array, end);
     }
-
-    @Override
-    public CharSequence subSequence(int start, int end) {
-      if ((start < 0) || (start > this.count) || (start > end) || (end > this.count)) {
-        throw new IndexOutOfBoundsException();
-      }
-      return new CharArrayCharSubSequence(this.array, start, end - start);
-    }
-
+    return new CharArrayFullSubSequence(this.array, start, end - start);
   }
 
+}
+
+
+final class CharArrayFullSubSequence implements CharSequence {
+
+  private final char[] array;
+  private final int offset;
+  private final int count;
+  private String stringValue;
+
+  CharArrayFullSubSequence(char[] array, int offset, int length) {
+    Objects.requireNonNull(array);
+    this.array = array;
+    this.offset = offset;
+    this.count = length;
+  }
+
+  @Override
+  public String toString() {
+    if (this.stringValue == null) {
+      this.stringValue = new String(this.array, this.offset, this.count);
+    }
+    return this.stringValue;
+  }
+
+  @Override
+  public int length() {
+    return this.count;
+  }
+
+  @Override
+  public char charAt(int index) {
+    if ((index < 0) || (index >= this.count)) {
+    throw new IndexOutOfBoundsException();
+  }
+    return this.array[this.offset + index];
+  }
+
+  @Override
+  public CharSequence subSequence(int start, int end) {
+    if ((start < 0) || (start > this.count) || (start > end) || (end > this.count)) {
+      throw new IndexOutOfBoundsException();
+    }
+    return new CharArrayFullSubSequence(this.array, start, end - start);
+  }
 
 }
